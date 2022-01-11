bdd887de3d76df2f00fdfe28ef17f983683c5ec2
CheeMinHao <cheemh3456@gmail.com>
2022-01-11 16:08:05 +0800

    fixed awards and reason text formatting

--- main.py
+++ main.py
@@ -1,80 +1,101 @@
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 
+
 def generate_certificate():
 
-    data = pd.read_csv('names.csv')
+    data = pd.read_csv("names.csv")
 
     name_list = data["Name"].tolist()
 
-    for i in name_list:
+    for i in range(len(name_list)):
 
         # Specify your certificate image route
-        im = Image.open('certificate.jpg')
+        im = Image.open("new_certificate.jpg")
 
         # Extract Image width and height
         image_width = im.width
         image_height = im.height
 
+        award = "Hello Coders Program"
+        reason = "yes"
+
         # Draw Image
         d = ImageDraw.Draw(im)
-
-        d = draw_name(d, i, image_width)
-        d = draw_award(d, i, image_width)
-        d = draw_reason(d, i, image_width)
+        d = draw_name(d, name_list[i], image_width)
+        d = draw_award(d, award, image_width)
+        d = draw_reason(d, reason, image_width)
 
         # Saving Certificate
-        # im.save("certificate_" + i + ".pdf")
-        im.save(f'certificates/certificate_{i}.pdf')
+        im.save(f"certificates/certificate_{name_list[i]}.pdf")
+
 
 def draw_name(drawing, name, image_width):
     # Specify Y-Axis Height, and Text Color (Adjust these according to your needs)
-    y_axis_height = 285
+    y_axis_height = 540
     text_color = (0, 137, 209)
 
     # Import text font (make sure text font is downloaded and in project root directory)
-    font = ImageFont.truetype("fonts/arial.ttf", 40)
+    font = ImageFont.truetype("fonts/Montserrat-Regular.ttf", 60)
 
     # Extracting text width
-    text_width, _ = drawing.textsize(name, font = font)
+    text_width, _ = drawing.textsize(name, font=font)
 
     # Writing text (Adjust calculation according to your needs)
-    drawing.text(((image_width - text_width) / 3, y_axis_height), name, fill = text_color, font = font)
+    drawing.text(
+        ((image_width - text_width) / 2, y_axis_height),
+        name,
+        fill=text_color,
+        font=font,
+    )
 
     return drawing
+
 
 def draw_award(drawing, name, image_width):
     # Specify Y-Axis Height, and Text Color (Adjust these according to your needs)
-    y_axis_height = 285
+    y_axis_height = 740
     text_color = (0, 137, 209)
 
     # Import text font (make sure text font is downloaded and in project root directory)
-    font = ImageFont.truetype("fonts/arial.ttf", 20)
+    font = ImageFont.truetype("fonts/arial.ttf", 50)
 
     # Extracting text width
-    text_width, _ = drawing.textsize(name, font = font)
+    text_width, _ = drawing.textsize(name, font=font)
 
     # Writing text (Adjust calculation according to your needs)
-    drawing.text(((image_width - text_width) / 3, y_axis_height), name, fill = text_color, font = font)
+    drawing.text(
+        ((image_width - text_width) / 2, y_axis_height),
+        name,
+        fill=text_color,
+        font=font,
+    )
 
     return drawing
+
 
 def draw_reason(drawing, name, image_width):
     # Specify Y-Axis Height, and Text Color (Adjust these according to your needs)
-    y_axis_height = 285
+    y_axis_height = 900
     text_color = (0, 137, 209)
 
     # Import text font (make sure text font is downloaded and in project root directory)
-    font = ImageFont.truetype("fonts/arial.ttf", 20)
+    font = ImageFont.truetype("fonts/arial.ttf", 30)
 
     # Extracting text width
-    text_width, _ = drawing.textsize(name, font = font)
+    text_width, _ = drawing.textsize(name, font=font)
 
     # Writing text (Adjust calculation according to your needs)
-    drawing.text(((image_width - text_width) / 3, y_axis_height), name, fill = text_color, font = font)
+    drawing.text(
+        ((image_width - text_width) / 2, y_axis_height),
+        name,
+        fill=text_color,
+        font=font,
+    )
 
     return drawing
 
+
 if __name__ == "__main__":
     generate_certificate()
-    print('Certificates Generated!')
+    print("Certificates Generated!")
