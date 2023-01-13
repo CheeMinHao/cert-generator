550f5d02c86119674266a0a14ceb0ee352258710
CheeMinHao <cheemh3456@gmail.com>
2023-01-13 16:56:56 +0800

    update

--- main.py
+++ main.py
@@ -3,53 +3,53 @@ import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 
 
-def generate_certificate(image, award, reason):
+def generate_certificate(image_list, award, reason, csv_list):
 
-    dir = "generated-certificates/"
-    for f in os.listdir(dir):
-        os.remove(os.path.join(dir, f))
+    for i in range(len(csv_list)):
+        data = pd.read_csv(f"csv/{csv_list[i]}.csv")
 
-    data = pd.read_csv("names.csv")
+        name_list = data["Name"].tolist()
+        team_list = data["Team"].tolist()
 
-    name_list = data["Name"].tolist()
 
-    for i in range(len(name_list)):
+        for j in range(len(name_list)):
 
-        # Specify your certificate image route
-        im = Image.open(f"templates/{image}")
+            # Specify your certificate image route
+            im = Image.open(f"templates/{image_list[i]}")
 
-        # Extract Image width and height
-        image_width = im.width
-        image_height = im.height
+            # Extract Image width and height
+            image_width = im.width
+            # image_height = im.height
 
-        # Draw Image
-        d = ImageDraw.Draw(im)
-        d = draw_name(d, name_list[i], image_width)
-        d = draw_award(d, award, image_width)
-        d = draw_date(d, reason, image_width)
+            # Draw Image
+            d = ImageDraw.Draw(im)
+            d = draw_name(d, name_list[j], image_width)
+            # d = draw_award(d, award, image_width)
+            # d = draw_date(d, reason, image_width)
 
-        # Saving Certificate
-        im.save(f"certificates/certificate_{name_list[i]}.pdf")
+            # Saving Certificate
+            im.save(f"generated-certificates/{team_list[j]}/certificate_{name_list[j]}.png")
 
 
 def draw_name(drawing, name, image_width):
     # Specify Y-Axis Height, and Text Color (Adjust these according to your needs)
-    y_axis_height = 285
+    y_axis_height = 600
 
     # Blue: (0, 137, 209)
     # Purple: (128, 0, 128)
     # Black: (0, 0, 0)
-    text_color = (0, 0, 0)
+    # White (255, 255, 255)
+    text_color = (226, 190, 118)
 
     # Import text font (make sure text font is downloaded and in project root directory)
-    font = ImageFont.truetype("fonts/Montserrat-Bold.ttf", 25)
+    font = ImageFont.truetype("fonts/TeanAeganRegular.ttf", 60)
 
     # Extracting text width
     text_width, _ = drawing.textsize(name, font=font)
 
     # Writing text (Adjust calculation according to your needs)
     drawing.text(
-        ((image_width - text_width) / 2 - 155, y_axis_height),
+        ((image_width - text_width) / 2, y_axis_height),
         name,
         fill=text_color,
         font=font,
--- param.py
+++ param.py
@@ -1,9 +1,11 @@
 from main import generate_certificate
-
+import os
+import shutil
+import pandas as pd
 
 def set_params():
     # Image (Certificate) name
-    image = "updated-certificate.png"
+    image_list = ['mdhack-participant.png', 'mdhack-3rd.png', 'mdhack-2nd.png', 'mdhack-1st.png', 'mdhack-mentor.png', 'mdhack-judge.png']
 
     # Award Name
     award = "Hello Coders Mentorship Program - S1 2022"
@@ -11,9 +13,24 @@ def set_params():
     # Reason/Date
     reason = "16th February - 8th April 2022"
 
-    generate_certificate(image, award, reason)
+    csv_list = ['names-participant', 'name-3rd', 'name-2nd', 'name-1st', 'names-mentor', 'names-judge']
+
+    generate_certificate(image_list, award, reason, csv_list)
+
+def create_folder():
+
+    dir = "generated-certificates/"
+    for f in os.listdir(dir):
+        shutil.rmtree(os.path.join(dir, f))
+
+    team_names = pd.read_csv("csv/teams.csv")
+    team_names = team_names["Team"].tolist()
+
+    for i in range(len(team_names)):
+        os.mkdir(os.path.join(dir, team_names[i]))
 
 
 if __name__ == "__main__":
+    create_folder()
     set_params()
     print("Certificates Generated!")
